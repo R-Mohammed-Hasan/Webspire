459ca7fac71d7b7b1c27db202347737307e25a25
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-07-19 21:07:57 +0530

    Added comment feature without Ajax

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@ class User < ApplicationRecord
   has_one_attached :user_profile
   has_one_attached :story
   has_many :likes
+  has_many :comments
 
   validates :user_name, presence: true, uniqueness: true
   validates :email, presence: true, uniqueness: true
