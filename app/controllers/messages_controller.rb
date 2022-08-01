72d9db144fbf136735e47bc8631ecb8733f59e4c
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-08-01 19:03:48 +0530

    added

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,20 +3,16 @@ class MessagesController < ApplicationController
   layout "home"
 
   def show
-<<<<<<< HEAD
-=======
     friends = @current_user.friends
     render "messages/message",locals: {friends: friends}
   end
 
   def message
     messages = Chatting.where(room_id: getRoom(params[:id]))
->>>>>>> bb13e75147763f67ff7f29a8817fa78b2f414971
     friends = @current_user.friends
     render "messages/message",locals: {friends: friends}
   end
 
-<<<<<<< HEAD
   def message
     if params[:id].split("_")[0] == @current_user.id.to_s or params[:id].split("_")[1] == @current_user.id.to_s
       messages = Chatting.where(room_id: getRoom(params[:id]))
@@ -25,8 +21,6 @@ class MessagesController < ApplicationController
     end
   end
 
-=======
->>>>>>> bb13e75147763f67ff7f29a8817fa78b2f414971
 
   def create
     message = Chatting.create(sender_id: @current_user.id, room_id: getRoom(params[:id]), message: params[:input])
