69f19a6171fdc396b773a84ef4e10fde77ad1e48
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-08-07 12:21:43 +0530

    Adding pagination

--- app/assets/coffeescripts/pagination.coffee
+++
@@ -1,7 +0,0 @@
-    if $('.pagination').length && $(".postsContainer").length
-        $(window).scroll ->
-                url = $('.pagination .next_page').attr('href')
-                if url && $(window).scrollTop() > $(document).height() - $(window).height() - 60
-                    $('.pagination').html('<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />')
-                    $.getScript(url)
-        $(window).scroll()
---
+++ app/assets/javascripts/pagination.js
@@ -0,0 +1,16 @@
+if ($(".pagination").length && $(".postsContainer").length) {
+    $(window).scroll(function() {
+        var url;
+        url = $(".pagination .next_page").attr("href");
+        if (
+            url &&
+            $(window).scrollTop() > $(document).height() - $(window).height() - 60
+        ) {
+            $(".pagination").html(
+                '<img src="/assets/ajax-loader.gif" alt="Loading..." title="Loading..." />'
+            );
+            return $.getScript(url);
+        }
+    });
+    $(window).scroll();
+}
