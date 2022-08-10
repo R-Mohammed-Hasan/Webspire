7a1c19b962e16fe5c005bc8d2a291d87309e5a93
R-Mohammed-Hasan <mohammed.hasan@fwsa.freshworks.com>
2022-08-10 19:39:36 +0530

    Adding Pagination

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_730_131_122) do
+ActiveRecord::Schema.define(version: 20_220_810_105_004) do
   create_table 'active_storage_attachments', charset: 'utf8mb4', collation: 'utf8mb4_0900_ai_ci', force: :cascade do |t|
     t.string 'name', null: false
     t.string 'record_type', null: false
